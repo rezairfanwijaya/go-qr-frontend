bb6dfdf0a64cefcd84100ef0287209582854144b
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-01 15:30:05 +0700

    add: color green

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
       'primary' : '#F6F8FD',
       'text-dark' : '#2B2B2B',
       'text-mute' : '#808080',
-      'orange' : '#F38704'
+      'orange' : '#F38704',
+      'white' : '#FFFFFF',
+      'green' : '#08A24D'
     }
   },
   plugins: [],

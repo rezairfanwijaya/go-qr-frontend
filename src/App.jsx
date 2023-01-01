54c59b237cc6828ce66f628ec1576d01fe581061
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-01 14:47:33 +0700

    add: navbar into main app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './style/style.css';
+
+function App() {
+  return (<>
+    <div className="app container mx-auto px-2 md:px-16 lg:px-36">
+      <Navbar />
+    </div>
+  </>
+  );
+}
+
+export default App;

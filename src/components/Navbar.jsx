fbb981d48c03a7fccf93f5a4a9d10eaf2efa32df
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-01-01 14:47:09 +0700

    add: navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import logo from '../assets/image/logo.png'
+
+const Navbar = () => {
+    return (<>
+        <div className="navbar flex justify-between py-2">
+
+            <div className="left">
+                <a href="/">
+                    <img src={logo} alt="logo" className='w-8 lg:w-7' />
+                </a>
+            </div>
+
+            <div className="right">
+                <a
+                    href="https://github.com/rezairfanwijaya/go-qr-frontend"
+                    target={'_blank'}
+                    rel="noreferrer"
+                >
+                    <div className="icon text-4xl lg:text-3xl">
+                        <ion-icon name="logo-github"></ion-icon>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+    </>);
+}
+
+export default Navbar;
